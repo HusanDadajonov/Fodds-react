00336c0d10eb352672f522e0479fa231a81af44a
am1rcek <xusandadajonov4385@gmail.com>
2022-02-07 18:47:27 +0500

    end

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import Bar from "../../container/Bar/Bar"
+function Home() {
+    return(
+        <section className="home">
+            <div className="home__row row">
+                <div className="home__col col-1 ps-0">
+                    <Bar />
+                </div>
+                <div className="home__col col-11">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Home 
